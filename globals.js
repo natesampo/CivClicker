7d7c9cba6cd7d0f622df0cb69443e07350069a6e
Nate Sampo <nathan.sampo@students.olin.edu>
2018-03-23 16:20:21 -0400

    Changed how jobs are created to be more scalable

--- globals.js
+++ globals.js
@@ -4,42 +4,38 @@ var year = 0;
 //Population
 var population = 10;
 var employedPopulation = 0;
-var unemployedPopulation = 100;
 
-//var foodGathering = 0;
-//var waterGetting = 0;
-
-var jobNames = ["foodGathering", "waterGetting"];
-var jobNums = [0, 0];
+var publicJobNames = [];
+var publicJobNums = [];
+var publicJobElements = [];
+var publicJobEffects = [];
 
 //Resources
 var food = 20;
 var water = 10;
 
-
+makePublicJob("FoodGathering", Math.round(population/2), function(workers) { food += workers/50 });
+makePublicJob("WaterGetting", Math.round(population/4), function(workers) { water += workers/25 });
+makePublicJob("ConsoleLogging", 1, function(workers) { for(var i=0;i<workers;i++) { console.log("hello") }});
 
 
 
 
 
 //Tab Navigation
-var i, tabcontent, tablinks;
-
-tabcontent = document.getElementsByClassName("tabcontent");
+var tabcontent = document.getElementsByClassName("tabcontent");
 tabcontent[0].style.display = "block";
-for(i = 1; i<tabcontent.length; i++) {
+for(var i = 1; i<tabcontent.length; i++) {
 	tabcontent[i].style.display = "none";
 }
 
-tablinks = document.getElementsByClassName("tablinks");
+var tablinks = document.getElementsByClassName("tablinks");
 tablinks[0].className += " active";
-for(i = 1; i<tablinks.length; i++) {
+for(var i = 1; i<tablinks.length; i++) {
 	tablinks[i].className = tablinks[i].className.replace(" active", "");
 }
 
 function openTab(evt, tabName) {
-	console.log(evt.currentTarget);
-
 	tabcontent = document.getElementsByClassName("tabcontent");
 	for(i = 0; i<tabcontent.length; i++) {
 		tabcontent[i].style.display = "none";
@@ -53,4 +49,89 @@ function openTab(evt, tabName) {
 	document.getElementById(tabName).style.display = "block";
 
 	evt.currentTarget.className += " active";
+}
+
+
+
+//Job Stuff
+function getPublicJobWorkers(jobName) {
+	for(var i=0;i<publicJobNames.length;i++) {
+		if(publicJobNames[i] == jobName) {
+			return publicJobNums[i];
+		}
+	}
+}
+
+function getPublicJobIndex(jobName) {
+	for(var i=0;i<publicJobNames.length;i++) {
+		if(publicJobNames[i] == jobName) {
+			return i;
+		}
+	}
+}
+
+function updatePublicJobs(jobName, delta) {
+	var tempIndex = getPublicJobIndex(jobName);
+	var tempNum = publicJobNums[tempIndex];
+	if(delta > 0) {
+		if(population - employedPopulation >= delta) {
+			publicJobNums[tempIndex] += delta;
+			employedPopulation += delta;
+		} else {
+			publicJobNums[tempIndex] += population - employedPopulation;
+			employedPopulation = population;
+		}
+	} else if(delta < 0) {
+		if(tempNum >= -delta) {
+			publicJobNums[tempIndex] += delta;
+			employedPopulation += delta;
+		} else {
+			employedPopulation -= publicJobNums[tempIndex];
+			publicJobNums[tempIndex] = 0;
+		}
+	}
+
+	publicJobElements[tempIndex].innerHTML = getPublicJobWorkers(jobName) + "<br><br>";
+}
+
+function makePublicJob(jobName, workers, effect) {
+	publicJobNames.push(jobName);
+	publicJobNums.push(0);
+	publicJobEffects.push(effect);
+
+	var newJobWrapper = document.createElement("newJobWrapper");
+	newJobWrapper.setAttribute("class", "jobWrapper");
+	newJobWrapper.innerHTML = "<br><br>" + jobName + ": ";
+	document.getElementById("centerColumn").appendChild(newJobWrapper);
+
+	var newJobCounter = document.createElement("newJobCounter");
+	newJobCounter.setAttribute('class', 'jobCounter');
+	newJobCounter.innerHTML = workers + "<br><br>";
+	newJobWrapper.appendChild(newJobCounter);
+	publicJobElements.push(newJobCounter);
+
+	var newJobAddButton = document.createElement("newJobAddButton");
+	newJobAddButton.setAttribute('class', 'addButton');
+	newJobAddButton.innerHTML = "Add";
+	newJobAddButton.type = "button";
+	newJobAddButton.addEventListener('click', function() { updatePublicJobs(jobName, 1) });
+	document.getElementById("centerColumn").appendChild(newJobAddButton);
+
+	var newJobRemoveButton = document.createElement("newJobRemoveButton");
+	newJobRemoveButton.setAttribute('class', 'removeButton');
+	newJobRemoveButton.innerHTML = "Remove";
+	newJobRemoveButton.type = "button";
+	newJobRemoveButton.addEventListener('click', function() { updatePublicJobs(jobName, -1) });
+	document.getElementById("centerColumn").appendChild(newJobRemoveButton);
+
+	updatePublicJobs(jobName, workers);
+}
+
+function removePublicJob(jobName) {
+	var tempIndex = getPublicJobIndex(jobName);
+	employedPopulation += publicJobNums[tempIndex];
+	publicJobNames.splice(tempIndex, 1);
+	publicJobNums.splice(tempIndex, 1);
+	publicJobElems.splice(tempIndex, 1);
+	publicJobEffects.splice(tempIndex, 1);
 }
--- main.js
+++ main.js
@@ -2,18 +2,14 @@ function cacheDOMElements() {
 	foodElement = document.getElementById("food");
 	waterElement = document.getElementById("water");
 	populationElement = document.getElementById("population");
-	foodGatheringElement = document.getElementById("foodGathering");
-	waterGettingElement = document.getElementById("waterGetting");
 	employedPopulationElement = document.getElementById("employedPopulation");
 	unemployedPopulationElement = document.getElementById("unemployedPopulation");
 	yearElement = document.getElementById("year");
 
 	foodElement.innerHTML = food;
 	populationElement.innerHTML = population;
-	foodGatheringElement.innerHTML = foodGathering;
-	waterGettingElement.innerHTML = waterGetting;
 	employedPopulationElement.innerHTML = employedPopulation;
-	unemployedPopulationElement.innerHTML = unemployedPopulation;
+	unemployedPopulationElement.innerHTML = population - employedPopulation;
 	yearElement.innerHTML = year;
 	waterElement.innerHTML = water;
 }
@@ -21,75 +17,33 @@ function cacheDOMElements() {
 var foodElement;
 var populationElement;
 var employedPopulationElement;
-var foodGatheringElement;
-var waterGettingElement;
-var employedPopulationElement
 var unemployedPopulationElement;
 var yearElement;
 var waterElement;
 
 cacheDOMElements();
 
-function addGatherFood() {
-	if(unemployedPopulation > 0) {
-		employedPopulation += 1;
-		unemployedPopulation -= 1;
-		foodGathering += 1;
-		foodGatheringElement.innerHTML = foodGathering.toString();
-		employedPopulationElement.innerHTML = employedPopulation.toString();
-		unemployedPopulationElement.innerHTML = unemployedPopulation.toString();
-	}
-}
-
-function removeGatherFood() {
-	if(foodGathering > 0) {
-		employedPopulation -= 1;
-		unemployedPopulation += 1;
-		foodGathering -= 1;
-		foodGatheringElement.innerHTML = foodGathering.toString();
-		employedPopulationElement.innerHTML = employedPopulation.toString();
-		unemployedPopulationElement.innerHTML = unemployedPopulation.toString();
-	}
-}
-
-function addGetWater() {
-	if(unemployedPopulation > 0) {
-		employedPopulation += 1;
-		unemployedPopulation -= 1;
-		waterGetting += 1;
-		waterGettingElement.innerHTML = waterGetting.toString();
-		employedPopulationElement.innerHTML = employedPopulation.toString();
-		unemployedPopulationElement.innerHTML = (population - employedPopulation).toString();
-	}
-}
-
-function removeGetWater() {
-	if(waterGetting > 0) {
-		employedPopulation -= 1;
-		unemployedPopulation += 1;
-		waterGetting -= 1;
-		waterGettingElement.innerHTML = waterGetting.toString();
-		employedPopulationElement.innerHTML = employedPopulation.toString();
-		unemployedPopulationElement.innerHTML = unemployedPopulation.toString();
-	}
-}
-
 window.setInterval(function() {
 
 	year += 1;
-	yearElement.innerHTML = year;
 
-	food += -(population/100) + (foodGathering/50);
-	foodElement.innerHTML = Math.round(food);
+	food -= population/100;
+	water -= population/100;
 
-	water += -(population/50) + (waterGetting/12.5);
-	waterElement.innerHTML = Math.round(water);
+	for (var i=0;i<publicJobEffects.length;i++) {
+		publicJobEffects[i](publicJobNums[i]);
+	}
 
 	if(food < 0) {
 		cannibalism();
 	}
 
-	unemployedPopulationElement.innerHTML = (population - employedPopulation).innerHTML;
+	foodElement.innerHTML = Math.round(food);
+	waterElement.innerHTML = Math.round(water);
+	populationElement.innerHTML = population;
+	employedPopulationElement.innerHTML = employedPopulation;
+	unemployedPopulationElement.innerHTML = population - employedPopulation;
+	yearElement.innerHTML = year;
 
 }, 100);
 
@@ -103,38 +57,13 @@ function cannibalism() {
 		console.log('Game over man!');
 		food = 0;
 		killed = 0;
-	} else if(unemployedPopulation >= killed) {
-		unemployedPopulation -= killed;
-	} else if(unemployedPopulation > 0) {
-		killed -= unemployedPopulation;
-		unemployedPopulation = 0;
-		employedPopulation -= killed;
+	} else if(population - employedPopulation >= killed) {
+		population -= killed;
+	} else if(population - employedPopulation > 0) {
+		killed -= population - employedPopulation;
+		population -= killed;
 		killed = 0;
 	} else {
 		employedPopulation -= killed;
 	}
-	populationElement.innerHTML = population;
-	unemployedPopulationElement.innerHTML = unemployedPopulation;
-	employedPopulationElement.innerHTML = employedPopulation;
-	foodElement.innerHTML = Math.round(food);
-}
-
-function getJobNumber(jobName) {
-	for(var i;i<jobNames.length;i++) {
-		if(jobNames[i] == jobName) {
-			return jobNums[i];
-		}
-	}
-}
-
-function getJobIndex(jobName) {
-	for(var i;i<jobNames.length;i++) {
-		if(jobNames[i] == jobName) {
-			return i;
-		}
-	}
-}
-
-function updateJobs(jobName, delta) {
-	jobNums[getJobIndex(jobName)] += delta;
 }
